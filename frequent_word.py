52d715b252482211a227e371cd928974525f58a8
inago-s <inago5173@gmail.com>
2021-12-28 15:51:49 +0000

    first commit

--- frequent_word.py
+++ frequent_word.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import japanize_matplotlib
 import pandas as pd
 
-con = sqlite3.connect('lanove_2020.db')
+con = sqlite3.connect('lanove_2020.sqlite3')
 cursor = con.cursor()
 nlp = spacy.load('ja_ginza_electra')
 
@@ -13,41 +13,105 @@ with open('stopword.txt', 'r') as f:
     stopword = f.read().split('\n')
 
 
-def get_noun_list(doc):
+def get_isekai_fantasy(doc):
     noun_list = []
     noun_flag = False
+    prefix_flag = False
     temp = ''
     for sent in doc.sents:
         for token in sent:
             tag = token.tag_.split('-')[0]
-            if noun_flag:
-                if tag == '名詞' or tag == '接尾辞':
+            if tag == '接頭辞':
+                if noun_flag and temp not in stopword:
+                    noun_list.append(temp)
+                noun_flag = False
+                prefix_flag = True
+                temp = token.orth_
+            elif tag == '名詞':
+                if prefix_flag:
                     temp += token.orth_
-                else:
+                    prefix_flag = False
+                    noun_flag = True
+                    continue
+                elif noun_flag:
+                    temp += token.orth_
+                    continue
+                temp = token.orth_
+                prefix_flag = False
+                noun_flag = True
+            elif tag == '接尾辞':
+                if noun_flag:
+                    temp += token.orth_
                     if temp not in stopword:
                         noun_list.append(temp)
-                        temp = ''
-                        noun_flag = False
+                prefix_flag = False
+                noun_flag = False
             else:
-                if tag == '名詞' or tag == '接頭辞':
+                if noun_flag and temp not in stopword:
+                    noun_list.append(temp)
+                    temp = ''
+                noun_flag = False
+                prefix_flag = False
+        if noun_flag and temp not in stopword:
+            noun_list.append(temp)
+
+    return noun_list
+
+
+def get_isekai(doc):
+    noun_list = []
+    noun_flag = False
+    prefix_flag = False
+    temp = ''
+    for sent in doc.sents:
+        for token in sent:
+            tag = token.tag_.split('-')[0]
+            if tag == '接頭辞':
+                if noun_flag and temp not in stopword:
+                    noun_list.append(temp)
+                noun_flag = False
+                prefix_flag = True
+                temp = token.orth_
+            elif tag == '名詞':
+                if prefix_flag:
                     temp += token.orth_
+                    prefix_flag = False
                     noun_flag = True
-        if not temp == '' and temp not in stopword:
+                    continue
+                elif noun_flag:
+                    noun_list.append(temp)
+                temp = token.orth_
+                prefix_flag = False
+                noun_flag = True
+            elif tag == '接尾辞':
+                if noun_flag:
+                    temp += token.orth_
+                    if temp not in stopword:
+                        noun_list.append(temp)
+                prefix_flag = False
+                noun_flag = False
+            else:
+                if noun_flag and temp not in stopword:
+                    noun_list.append(temp)
+                    temp = ''
+                noun_flag = False
+                prefix_flag = False
+        if noun_flag and temp not in stopword:
             noun_list.append(temp)
 
     return noun_list
 
 
 word_list = []
-for row in cursor.execute('select * from book_info'):
-    doc = nlp(row[1])
-    word_list.extend(get_noun_list(doc))
+for row in cursor.execute('select content from book_info'):
+    doc = nlp(row[0])
+    word_list.extend(get_isekai_fantasy(doc))
 
 
 counter = Counter(word_list)
 df = pd.DataFrame.from_dict(counter, orient='index', columns=['frequent'])
 df = df.sort_values('frequent', ascending=False)
-df.to_csv('word_2020.csv')
+df.to_csv('word_2020_noun_isekai_fantasy.csv')
 
 for data in df.head(20).itertuples():
     plt.bar(data.Index, data.frequent)
@@ -56,5 +120,5 @@ plt.grid(axis='y')
 plt.xlabel('単語')
 plt.xticks(rotation=90)
 plt.ylabel('出現回数')
-plt.savefig('word_2021.png', facecolor="azure",
+plt.savefig('word_2020_noun_isekai_fantasy.png',
             bbox_inches='tight', pad_inches=0, dpi=300)
